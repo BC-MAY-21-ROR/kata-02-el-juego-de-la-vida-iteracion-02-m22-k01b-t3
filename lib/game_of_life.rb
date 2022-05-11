733e1d9e8cfe97db2345fe33f01365a2d65ec358
Victoriano <mytacam78@gmail.com>
2022-05-11 16:33:21 -0600

    Copy matrix and create new_matrix Co-authored-by  d1eg0dev Co-authored-by  HikingCarrot7

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -25,7 +25,78 @@ class Game
                 end    
             end
         end        
+    end
 
+    # first generation
+    def next_generation
+        print_matrix
+        new_matrix = create_matrix
+
+        new_matrix.each_with_index.map do |array, index_array|
+            array.each_with_index.map do |celula, index_celula|
+            right = []
+            left = []
+            top = []
+            bottom = []
+
+            #Check left to right
+            if index_celula.zero?
+                right.push(array[index_celula + 1])
+            elsif index_celula == (array.length - 1)
+                left.push(array[index_celula - 1])
+            else
+                left.push(array[index_celula - 1])
+                right.push(array[index_celula + 1])
+            end
+
+                        #Check top
+            if index_array.zero?
+                'none'
+            elsif index_celula.zero?
+                top.push(new_matrix[index_array - 1][index_celula], new_matrix[index_array - 1][index_celula + 1])
+            elsif index_celula == array.length - 1
+                top.push(new_matrix[index_array - 1][index_celula - 1], new_matrix[index_array - 1][index_celula])
+            else
+                top.push(new_matrix[index_array - 1][index_celula - 1], new_matrix[index_array - 1][index_celula],
+                new_matrix[index_array - 1][index_celula + 1])
+            end
+            
+
+            #Check bottom
+            if index_array == (new_matrix.length - 1)
+                'none'
+            elsif index_celula.zero?
+                bottom.push(new_matrix[index_array + 1][index_celula], new_matrix[index_array + 1][index_celula + 1])
+            elsif index_celula == (array.length - 1)
+                bottom.push(new_matrix[index_array + 1][index_celula - 1], new_matrix[index_array + 1][index_celula])
+            else
+                bottom.push(new_matrix[index_array + 1][index_celula - 1],new_matrix[index_array + 1][index_celula], new_matrix[index_array + 1][index_celula + 1])
+            end
+            
+            neighboors=left+right+top+bottom
+            total_neighboors = neighboors.sum
+
+            #Rules implementation
+            if celula==1
+                if  total_neighboors<2 || total_neighboors>3
+                new_matriz[index_array][index_celula]=0
+                modify_matriz[index_array][index_celula]="."
+                elsif total_neighboors==2 || total_neighboors==3
+                new_matriz[index_array][index_celula]=1
+                modify_matriz[index_array][index_celula]="*"
+                end
+            else #celula==0
+                if total_neighboors==3
+                new_matriz[index_array][index_celula]=1
+                modify_matriz[index_array][index_celula]="*"
+                else
+                new_matriz[index_array][index_celula]=0
+                modify_matriz[index_array][index_celula]="."
+
+                end
+            end
+        end
+        end
     end
 
     def print_matrix
@@ -39,5 +110,6 @@ class Game
     tablero = Game.new(width,height)
     tablero.create_matrix
     tablero.print_matrix
+    tablero.next_generation
 end
 
